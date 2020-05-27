4aa8710c33afa00cd762dff670aa168149dd99aa
JongWanKim <46095454+oen142@users.noreply.github.com>
2020-05-27 23:39:20 +0900

    Initial commit

---
+++ src/test/java/springsecurity/white/WhiteApplicationTests.java
@@ -0,0 +1,13 @@
+package springsecurity.white;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
